404b7f5180b1405118e3c4092b24db09476c5f88
garytayl <132408374+garytayl@users.noreply.github.com>
2025-06-02 19:41:51 -0400

    fixing the port

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ app.get('/', (req, res) => {
   res.send('PokéPulse bot is live!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
